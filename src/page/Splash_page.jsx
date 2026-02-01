abf67842e7c03f5438390b1b6e0d9d62c136330d
metachemist <meta.chemist.22@gmail.com>
2026-02-01 15:54:28 +0500

    fix: update background color of Splash_page component for better visibility

--- src/page/Splash_page.jsx
+++ src/page/Splash_page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import splash_image from '/Splash_image.png'
 const Splash_page = () => {
   return (
-    <div className='w-full h-screen flex justify-center items-center bg-[#EEEEEE] '>
+    <div className='w-full h-screen flex justify-center items-center bg-[#c2b38c] '>
         <img src={splash_image} alt="" srcset="" className='animate-pulse'/>
     </div>
   )
